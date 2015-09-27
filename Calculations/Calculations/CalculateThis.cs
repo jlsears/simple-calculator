adcbe10cb75b42a498c74a25c5e011e90f7233fa
jlsears <jen.lauren.sears@gmail.com>
2015-09-27 12:22:36 -0500

    initial prompt functioning

---
+++ Calculations/Calculations/CalculateThis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    public static class CalculateThis
+    {
+        public static void Init()
+        {
+            int counter = 0;
+
+            Console.Write("[" + counter + "]" + "> ");
+            string desiredCalc = Console.ReadLine();
+            Console.WriteLine("We're going to calculate " + desiredCalc);
+            Console.ReadLine();
+        }
+    }
+}
