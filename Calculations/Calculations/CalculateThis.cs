dc6b444ccf8b8e58136fb311273cf1c9aa9efb56
jlsears <jen.lauren.sears@gmail.com>
2015-09-27 18:18:08 -0500

    numbers and delimiter isolated

--- Calculations/Calculations/CalculateThis.cs
+++ Calculations/Calculations/CalculateThis.cs
@@ -11,10 +11,30 @@ namespace Calculations
         public static void Init()
         {
             int counter = 0;
+            var theDelimiters = new[] { '+', '-', '*', '/', '%' };
 
             Console.Write("[" + counter + "]" + "> ");
             string desiredCalc = Console.ReadLine();
             Console.WriteLine("We're going to calculate " + desiredCalc);
+           
+
+            string[] parsedInfo = desiredCalc.Split(theDelimiters);
+
+            string firstPart = "filler";
+            string secondPart = "filler";
+             
+            foreach (string s in parsedInfo)
+            {
+                firstPart = parsedInfo[0];
+                secondPart = parsedInfo[1];
+            }
+
+            Console.WriteLine("We have " + firstPart + " and " + secondPart + " now.");
+
+
+            var foundIt = desiredCalc.IndexOfAny(theDelimiters);
+            Console.WriteLine("Here's our delimiter: " + desiredCalc[foundIt]);
+
             Console.ReadLine();
         }
     }
