66bb6520aec3b46d20bb59ce1b07bf3f60b012f2
jlsears <jen.lauren.sears@gmail.com>
2015-09-29 17:10:48 -0500

    infinite loop and counter incrementing for addition

--- Calculations/Calculations/CalculateThis.cs
+++ Calculations/Calculations/CalculateThis.cs
@@ -11,56 +11,85 @@ namespace Calculations
         public static void Init()
         {
             int counter = 0;
-            var theDelimiters = new[] { '+', '-', '*', '/', '%' };
-            var prompt = "[" + counter + "]" + "> ";
+            var theirReply = true;
 
-            Console.Write(prompt);
-            string desiredCalc = Console.ReadLine();
-            Console.WriteLine("We're going to calculate " + desiredCalc);
-           
-
-            string[] parsedInfo = desiredCalc.Split(theDelimiters);
-
-            string firstPart = "filler";
-            string secondPart = "filler";
-             
-            foreach (string s in parsedInfo)
+            while (theirReply)
             {
-                firstPart = parsedInfo[0];
-                secondPart = parsedInfo[1];
+
+                try
+                {
+                    var theDelimiters = new[] { '+', '-', '*', '/', '%' };
+                    var prompt = "[" + counter + "]" + "> ";
+
+                    Console.Write(prompt);
+                    string desiredCalc = Console.ReadLine();
+                    Console.WriteLine("We're going to calculate " + desiredCalc);
+
+
+                    string[] parsedInfo = desiredCalc.Split(theDelimiters);
+
+                    string firstPart = "filler";
+                    string secondPart = "filler";
+
+                    foreach (string s in parsedInfo)
+                    {
+                        firstPart = parsedInfo[0];
+                        secondPart = parsedInfo[1];
+                    }
+
+                    int firstAgain = Convert.ToInt32(firstPart);
+                    int secondAgain = Convert.ToInt32(secondPart);
+
+                    Console.WriteLine("We have " + firstPart + " and " + secondPart + " now.");
+
+
+                    var foundIt = desiredCalc.IndexOfAny(theDelimiters);
+                    var ourComputation = desiredCalc[foundIt];
+                    Console.WriteLine("Here's our delimiter: " + ourComputation);
+
+                    switch (ourComputation)
+                    {
+                        case '+':
+                            Console.WriteLine("= " + (firstAgain + secondAgain));
+                            counter = counter + 1;
+                            string reply = Console.ReadLine();
+                            switch(reply)
+                            {
+                                case "quit":
+                                    return;
+                                case "exit":
+                                    return;
+                                default:
+                                    break;
+                            }
+
+                            break;
+                        case '-':
+                            Console.WriteLine("= " + (firstAgain - secondAgain));
+                            Console.ReadLine();
+                            break;
+                        case '*':
+                            Console.WriteLine("= " + (firstAgain * secondAgain));
+                            Console.ReadLine();
+                            break;
+                        case '/':
+                            Console.WriteLine("= " + (firstAgain / secondAgain));
+                            Console.ReadLine();
+                            break;
+                        case '%':
+                            Console.WriteLine("= " + (firstAgain % secondAgain));
+                            Console.ReadLine();
+                            break;
+                    }
+
+                    Console.ReadLine();
+                }
+                catch
+                {
+                    //something here
+                }
             }
-
-            int firstAgain = Convert.ToInt32(firstPart);
-            int secondAgain = Convert.ToInt32(secondPart);
-
-            Console.WriteLine("We have " + firstPart + " and " + secondPart + " now.");
-
-
-            var foundIt = desiredCalc.IndexOfAny(theDelimiters);
-            var ourComputation = desiredCalc[foundIt];
-            Console.WriteLine("Here's our delimiter: " + ourComputation);
-
-            switch (ourComputation)
-            {
-                case '+' :
-                    Console.WriteLine(firstAgain + secondAgain);
-                    Console.ReadLine();
-                    break;
-                case '-':
-                    Console.WriteLine(firstAgain - secondAgain);
-                    Console.ReadLine();
-                    break;
-                case '*':
-                    Console.WriteLine(firstAgain * secondAgain);
-                    Console.ReadLine();
-                    break;
-                case '/':
-                    Console.WriteLine(firstAgain / secondAgain);
-                    Console.ReadLine();
-                    break;
-            }
-
-            Console.ReadLine();
+            //Init();
         }
     }
 }
